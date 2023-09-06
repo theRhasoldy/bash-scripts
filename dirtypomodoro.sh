05301032a30d8bbee12e508ccfdc9337aff30915
Nour El Din Shady <rhasoldy@gmail.com>
2023-09-06 06:05:27 +0300

    Added colorpicker and dirty pomodoro scripts

---
+++ dirtypomodoro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+timer -f -n Work 45m \
+&& (espeak "Take a break you brilliant bastard" \
+& notify-send --app-name="Dirty Pomodoro" "Break" "Take a break")
+
+timer -f -n Break 15m \
+&& (espeak "Get back to work you lazy bitch" \
+& notify-send --app-name="Dirty Pomodoro" "Work" "Get back to work")
